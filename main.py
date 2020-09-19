2e6ac86057b9143aa596706548fbe09f0a33fbde
unknown <valera.kkoz2@gmail.com>
2020-09-20 02:41:55 +0300

    Now I can draw a block!

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame
+import os
 
+from block import Block
+
+#constants
+WIDTH = 800
+HEIGHT = 450
+
+SCALE = 5 #scale of textures
+
+#initializating pygame
 pygame.init()
 
-w = 800
-h = 450
-screen = pygame.display.set_mode((w, h))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Minecraft 2d")
 
 clock = pygame.time.Clock()
 
-alive = True
+#initialazing textures
+textures = {"blocks":{}}
+for blockName in os.listdir("data/textures/blocks/"):
+	if blockName != "desktop.ini":
+		textures["blocks"].update({blockName[:-4]:pygame.transform.scale(pygame.image.load("data/textures/blocks/"+blockName), (16*SCALE, 16*SCALE))})
 
+#generating map
+blocks = []
+blocks.append(Block(1, 2, "grass"))
+
+#main cycle
+alive = True
 while alive:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -18,9 +36,11 @@ while alive:
 
 
 
-	screen.fill((31, 31, 31))
+	screen.fill((63, 127, 191))
 
-
+	#drawing blocks
+	for block in blocks:
+		screen.blit(textures["blocks"][block.name], (block.x*16*SCALE, block.y*16*SCALE))
 
 	pygame.display.update()
 
